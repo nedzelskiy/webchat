b22a55f2feaf9844fb376317d6413dd5072aada3
Pavel Nedzelskiy <pavel.nedzelskiy@gmail.com>
2015-12-07 23:13:00 +0200

    smiles added, fixed scrolling

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,30 +1,57 @@
-var socket = io.connect('http://' + location.hostname + ':' + location.port);
-var chat = $(".panel-info .media-list");
-var userTable = $(".panel-primary .media-list");
-var userTableTemplate = _.template($("#user-table-template").text());
-var sysMessageTemplate = _.template($("#system-message-template").text());
-var messageTemplate = _.template($("#message-template").text());
-var id = $("input[name='id']").val();
-var userstatus = $("input[name='userstatus']").val();
-var userCount = $(".users_count");
-var send = $(".btn.send");
-var inputText = $(".input-group").find("input[type='text']");
+var  socket = io.connect('http://' + location.hostname + ':' + location.port)
+    ,chat = $(".panel-info .media-list")
+    ,smiles = $("#smiles")
+    ,smilesBtn = $(".smile-show-btn")
+    ,panelBody = $('.panel-info .panel-body')
+    ,userTable = $(".panel-primary .media-list")
+    ,userTableTemplate = _.template($("#user-table-template").text())
+    ,sysMessageTemplate = _.template($("#system-message-template").text())
+    ,messageTemplate = _.template($("#message-template").text())
+    ,id = $("input[name='id']").val()
+    ,userstatus = $("input[name='userstatus']").val()
+    ,userCount = $(".users_count")
+    ,send = $(".btn.send")
+    ,inputText = $(".input-group").find("input[type='text']")
+    ;
 
 
 $(window).unload(function(){
     socket.disconnect();
 });
 
+smilesBtn.click(function(){
+    if ('none' === smiles.css('display')) {
+        openSmilesBox();
+    } else {
+        closeSmilesBox();
+    }
+});
+smiles.find('img').click(function(){
+    inputText.val(inputText.val()+' ::sm:'+$(this).attr('data-name'));
+    inputText.focus();
+});
+
+function closeSmilesBox(){
+    smiles.slideUp(500);
+    inputText.focus();
+};
+function openSmilesBox(){
+    smiles.slideDown(500);
+    inputText.focus();
+};
+
 function throwSysMessage(data){
     var sysmess = $(sysMessageTemplate(data));
     chat.append(sysmess);
-    sysmess.fadeIn(800);
+    sysmess.stop(true).fadeIn(800);
+    panelBody.stop(true).animate({ scrollTop: panelBody.prop('scrollHeight') }, 800);
 };
 
 function throwMessage(data){
     var mess = $(messageTemplate(data));
     chat.append(mess);
-    mess.fadeIn(800);
+    mess.stop(true).fadeIn(800);
+    panelBody.stop(true).animate({ scrollTop: panelBody.prop('scrollHeight') }, 800);
 };
 
 function addUser(data){
@@ -88,15 +115,20 @@ socket.on('message', function(data){
     }
 });
 socket.on('disconnect', function(){
+});
+
+socket.on('killsession',function(){
     alert("You have been disconnected from server!");
     document.location.assign("/");
 });
 
 send.click(function(){
     sendMessage(inputText.val());
+    closeSmilesBox();
 });
 inputText.keyup(function(event) {
     if(event.keyCode==13) {
         sendMessage($(this).val());
+        closeSmilesBox();
     }
 });
